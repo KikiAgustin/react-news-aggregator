e41090a0ac68ee10e95d2da2d63b6178508b2f92
kikiAgustin <kikiagustin62@gmail.com>
2021-02-27 07:08:48 +0600

    membuat layout header

--- starter/pages/about.js
+++ starter/pages/about.js
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import Avatar from '@material-ui/core/Avatar';
+import Chip from '@material-ui/core/Chip';
 
 const About = () => {
     return (
@@ -8,6 +10,10 @@ const About = () => {
             <Link href="/" >
                 <a>Menuju Ke Halaman Home</a>
             </Link>
+
+            <Avatar alt="Remy Sharp" src="https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/38/38c2bca1ea446adea699898e8188c40669b77176" />
+
+            <Chip label="Mencoba Chip" />
 
         </div>
     )
